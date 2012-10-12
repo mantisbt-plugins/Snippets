mantisbt-plugins/Snippets

commit 32eaf8c808685b630efe2dd1aff9b1f70a667357

Replace '&' by '&amp;' in plugin URLs for global links
Use of '&' triggers an XML error "not well formed" in Mantis 1.3

A new static function Snippet::global_url was added to generate the URLs
as appropriate.

Snippets/Snippets.API.php
@@ -326,5 +326,12 @@ class Snippet {
 
 		return $snippets;
 	}
+
+	public static function global_url($p_is_global = true) {
+		if ($p_is_global) {
+			return '&amp;global=true';
+		}
+		return '';
+	}
 }
 

Snippets/Snippets.php
@@ -61,7 +61,7 @@ class SnippetsPlugin extends MantisPlugin {
 
 	public function menu_manage($event, $user_id) {
 		if (access_has_global_level(plugin_config_get("edit_global_threshold"))) {
-			$page = plugin_page("snippet_list") . "&global=true";
+			$page = plugin_page("snippet_list") . Snippet::global_url();
 			$label = plugin_lang_get("list_global_title");
 
 			return "<a href=\"{$page}\">{$label}</a>";

Snippets/pages/snippet_create.php
@@ -28,5 +28,5 @@ $snippet = new Snippet(0, $name, $value, $user_id);
 $snippet->save();
 
 form_security_purge("plugin_snippets_create");
-print_successful_redirect(plugin_page("snippet_list", true) . ($global ? "&global=true" : ""));
+print_successful_redirect(plugin_page("snippet_list", true) . Snippet::global_url($global));
 

Snippets/pages/snippet_list_action.php
@@ -21,7 +21,7 @@ $snippet_list = gpc_get_int_array("snippet_list", array());
 
 if (count($snippet_list) < 1) {
 	form_security_purge("plugin_Snippets_list_action");
-	print_header_redirect(plugin_page("snippet_list", true) . ($global ? "&global=true" : ""));
+	print_header_redirect(plugin_page("snippet_list", true) . Snippet::global_url($global));
 }
 
 $snippets = Snippet::load_by_id($snippet_list, $user_id);
@@ -41,7 +41,7 @@ if ($action == "delete") {
 	Snippet::delete_by_id(array_keys($snippets), $user_id);
 
 	form_security_purge("plugin_Snippets_list_action");
-	print_successful_redirect(plugin_page("snippet_list", true) . ($global ? "&global=true" : ""));
+	print_successful_redirect(plugin_page("snippet_list", true) . Snippet::global_url($global));
 
 ### EDIT
 } elseif ($action == "edit") {
@@ -108,7 +108,7 @@ if ($action == "delete") {
 	}
 
 	form_security_purge("plugin_Snippets_list_action");
-	print_successful_redirect(plugin_page("snippet_list", true) . ($global ? "&global=true" : ""));
+	print_successful_redirect(plugin_page("snippet_list", true) . Snippet::global_url($global));
 
 }
 

commit ccecc122a3a5bf00307770c9c430a9a7559ebc3c

Editing/Deleting global snippet redirects to 'My Snippets'
Regression introduced by 32eaf8c808685b630efe2dd1aff9b1f70a667357; using
&amp; causes an incorrect URL to be generated for the submit buttons.

Now using string_html_specialchars() to display the menu's href.

Fixes #1

Snippets/Snippets.API.php
@@ -329,7 +329,7 @@ class Snippet {
 
 	public static function global_url($p_is_global = true) {
 		if ($p_is_global) {
-			return '&amp;global=true';
+			return '&global=true';
 		}
 		return '';
 	}

Snippets/Snippets.php
@@ -64,7 +64,7 @@ class SnippetsPlugin extends MantisPlugin {
 			$page = plugin_page("snippet_list") . Snippet::global_url();
 			$label = plugin_lang_get("list_global_title");
 
-			return "<a href=\"{$page}\">{$label}</a>";
+			return '<a href="' . string_html_specialchars( $page ) . '">' . $label . '</a>';
 		}
 	}
 