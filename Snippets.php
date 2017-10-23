bf02b4e0ba1bf684a7605c9f072248c1d0ee093a
Damien Regad <dregad@mantisbt.org>
2017-10-24 01:48:48 +0200

    Use REST API instead of xmlhttprequest
    XHR was deprecated in Mantis 2.3.0.
    
    Publish 3 REST API endpoints to replace the corresponding XHR functions:
    
    - xmlhttprequest_plugin_snippets_data
      => ./plugins/Snippets/list and ./plugins/Snippets/list/{bug_id}
    - xmlhttprequest_plugin_snippets_help
      => ./plugins/Snippets/help
    
    Fixes #16

--- Snippets.php
+++ Snippets.php
@@ -45,6 +45,8 @@ class SnippetsPlugin extends MantisPlugin {
 			"EVENT_MENU_MANAGE" => "menu_manage",
 
 			"EVENT_LAYOUT_RESOURCES" => "resources",
+
+			'EVENT_REST_API_ROUTES' => 'routes',
 		);
 	}
 
@@ -90,6 +92,27 @@ class SnippetsPlugin extends MantisPlugin {
 			<link rel="stylesheet" type="text/css" href="' . plugin_file("snippets.css") . '"/>';
 	}
 
+	/**
+	 * Add the RESTful routes handled by this plugin.
+	 *
+	 * @param string $p_event_name The event name
+	 * @param array  $p_event_args The event arguments
+	 * @return void
+	 */
+	public function routes( $p_event_name, $p_event_args ) {
+		$t_app = $p_event_args['app'];
+		$t_plugin = $this;
+		$t_app->group(
+			plugin_route_group(),
+			function() use ( $t_app, $t_plugin ) {
+				$t_app->get( '/help', [$t_plugin, 'route_help'] );
+
+				$t_app->get( '/data', [$t_plugin, 'route_data'] );
+				$t_app->get( '/data/{bug_id}', [$t_plugin, 'route_data'] );
+			}
+		);
+	}
+
 	public function schema() {
 		return array(
 			# 2010-03-18
@@ -101,5 +124,95 @@ class SnippetsPlugin extends MantisPlugin {
 				value		XL		NOTNULL
 				")),
 		);
+	}
+
+	/**
+	 * RESTful route for Snippets Pattern Help (tooltip).
+	 *
+	 * Returned JSON structure
+	 *   - {string} title
+	 *   - {string} text
+	 *
+	 * @param Psr\Http\Message\ServerRequestInterface $request
+	 * @param Psr\Http\Message\ResponseInterface $response
+	 * @param array $args
+	 * @return Psr\Http\Message\ResponseInterface
+	 */
+	public function route_help($request, $response, $args) {
+		$t_help = array(
+			'title' => plugin_lang_get( 'pattern_title', $this->basename ),
+			'text'  => plugin_lang_get( 'pattern_help', $this->basename ),
+		);
+		return $response
+			->withStatus( HTTP_STATUS_SUCCESS )
+			->withJson( $t_help );
+	}
+
+	/**
+	 * RESTful route for Snippets data.
+	 *
+	 * Returned JSON structure:
+	 * - {string}     version  - Plugin version
+	 * - {string}     selector - Configured jQuery selector for textareas
+	 * - {string}     label    - Language string for Snippets select's label
+	 * - {string}     default  - Language string for Snippets select's default option
+	 * - {null|array} snippets - List of snippets, with following structure:
+	 *   - {int}      id
+	 *   - {int}      user_id
+	 *   - {int}      type
+	 *   - {string}   name     - Snippet title
+	 *   - {string}   value    - Snippet text
+	 *
+	 * @param Psr\Http\Message\ServerRequestInterface $request
+	 * @param Psr\Http\Message\ResponseInterface $response
+	 * @param array $args [bug_id = Bug Id for patterns replacement]
+	 * @return Psr\Http\Message\ResponseInterface
+	 */
+	public function route_data( $request, $response, $args) {
+		plugin_push_current($this->basename);
+
+		# Set the reference Bug Id for placeholders replacements
+		if( isset( $args['bug_id'] ) ) {
+			$t_bug_id = (int)$args['bug_id'];
+		} else {
+			$t_bug_id = 0;
+		}
+
+		# Load snippets available to the user
+		$t_use_global = access_has_global_level( plugin_config_get( 'use_global_threshold' ) );
+		$t_user_id = -1;
+		if( access_has_global_level( plugin_config_get( 'edit_own_threshold' ) ) ) {
+			$t_user_id = auth_get_current_user_id();
+		}
+		$t_snippets = Snippet::load_by_type_user( 0, $t_user_id, $t_use_global );
+		$t_snippets = Snippet::clean( $t_snippets, 'form', $t_bug_id );
+
+		# Split names of textareas found in 'textarea_names' option, and
+		# make an array of "textarea[name='FIELD_NAME']" strings
+		$t_selectors = array_map(
+			function($name) {
+				return "textarea[name='$name']";
+			},
+			Snippet::get_configured_field_names()
+		);
+
+		$t_data = array(
+			'version' => SnippetsPlugin::VERSION,
+			# return configured jQuery selectors for textareas in "selector" field
+			'selector' => implode( ',', $t_selectors ),
+			'label' => plugin_lang_get( 'select_label' ),
+			'default' => plugin_lang_get( 'select_default' ),
+		);
+
+		# Arrange the available snippets into the data array
+		foreach( $t_snippets as $t_snippet ) {
+			$t_data['snippets'][$t_snippet->id] = $t_snippet;
+		}
+
+		plugin_pop_current();
+
+		return $response
+			->withStatus( HTTP_STATUS_SUCCESS )
+			->withJson( $t_data );
 	}
 }
