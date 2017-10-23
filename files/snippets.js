mantisbt-plugins/Snippets

commit 0dfa8b19f9ffe356b99505575e723e6652cc1162

Code cleanup in snippets.js
- remove implicit variable declarations
- fix various JSHint warnings
- fix indentation
- replace for-in by jQuery.each

files/snippets.js
@@ -3,112 +3,115 @@
 // Licensed under the MIT license
 
 jQuery(document).ready(function($) {
-		var SnippetsLangArray = null;
+	"use strict";
 
-		/**
-		 * Handle retrieving and storing language strings from the server.
-		 */
-		function SnippetsLang(str) {
-			if (SnippetsLangArray == null) {
-				xhr = $.ajax({
-					async: false,
-					dataType: "json",
-					url: "xmlhttprequest.php?entrypoint=plugin_snippets_text",
-					success: function(data) {SnippetsLangArray = data;}
-					});
-			}
+	var SnippetsLangArray = null;
 
-			return SnippetsLangArray[str];
+	/**
+	 * Handle retrieving and storing language strings from the server.
+	 */
+	function SnippetsLang(str) {
+		if (SnippetsLangArray === null) {
+			$.ajax({
+				async: false,
+				dataType: "json",
+				url: "xmlhttprequest.php?entrypoint=plugin_snippets_text",
+				success: function(data) {SnippetsLangArray = data;}
+			});
 		}
 
-		// Snippet list behaviors
-		$("input.snippets_select_all").change(function(){
-				$("input[name='snippet_list[]']").prop("checked", $(this).prop("checked"));
-			});
+		return SnippetsLangArray[str];
+	}
 
-		// Snippet pattern help
-		$(".snippetspatternhelp").each(function() {$(this).simpletip({
-				content: SnippetsLang("pattern_help"),
-				baseClass: "snippetsTooltip",
-				fixed: false,
-				offset: [20, 20],
-				});
-			});
+	// Snippet list behaviors
+	$("input.snippets_select_all").change(function(){
+		$("input[name='snippet_list[]']").prop("checked", $(this).prop("checked"));
+	});
 
-		/**
-		 * Primary Snippets functionality.
-		 * Use an AJAX request to retrieve the user's available snippets, and
-		 * then insert select boxes into the DOM for each supported textarea.
-		 */
-		function SnippetsInit() {
-			function SnippetsUI(data) {
-				var textarrays = data;
+	// Snippet pattern help
+	$(".snippetspatternhelp").each(function() {
+		$(this).simpletip({
+			content: SnippetsLang("pattern_help"),
+			baseClass: "snippetsTooltip",
+			fixed: false,
+			offset: [20, 20]
+		});
+	});
 
-				$(data.selector).each(function(index) {
-						var textarea_name = $(this).attr("name");
-						var textarea = $(this);
+	/**
+	 * Primary Snippets functionality.
+	 * Use an AJAX request to retrieve the user's available snippets, and
+	 * then insert select boxes into the DOM for each supported textarea.
+	 */
+	function SnippetsInit() {
+		function SnippetsUI(data) {
+			var textarrays = data;
 
-						try {
+			$(data.selector).each(function(index) {
+				var textarea = $(this);
+				var textarea_name = textarea.attr("name");
 
-						snippets = textarrays["texts"];
-						if (snippets != null) {
-							label = $("<label>" + SnippetsLang("label") + " </label>");
+				try {
+					var snippets = textarrays["texts"];
+					if (snippets !== null) {
+						// Create Snippets select
+						var select = $("<select></select>");
+						select.append("<option title='' value=''>" + SnippetsLang("default") + "</option>");
 
-							select = $("<select></select>");
-							select.append($("<option title='' value=''>" + SnippetsLang("default") + "</option>"));
+						$.each(snippets, function(key, snippet) {
+							// Escape single quotes
+							var value = snippet.value.replace(/'/g, "&#39;");
 
-							for (snippetid in snippets) {
-								snippet = snippets[snippetid];
-								// Escape single quotes
-								value = snippet.value.replace(/'/g, "&#39;" );
+							select.append(
+								"<option value='" + value + "' title='" + value + "'>" + snippet.name + "</option>"
+							);
+						});
 
-								option = $("<option value='" + value + "' title='" + value + "'>" + snippet.name + "</option>");
-								select.append(option);
-							}
+						select.change(function() {
+							var text = $(this).val();
+							textarea.textrange('replace', text);
+							$(this).val("");
+						});
 
-							select.change(function() {
-									text = $(this).val();
-									textarea.textrange('replace', text);
-									$(this).val("");
-								});
-							label.append(select);
+						var label = $("<label>" + SnippetsLang("label") + " </label>");
+						label.append(select);
 
-							$(this).before(label);
-							$(this).before('<div class="space-4"></div>');
+						$(this).before(label);
+						$(this).before('<div class="space-4"></div>');
 
-							$(this).parent("td").removeClass("center");
-						}
-
-						} catch(e) {
-							alert(e);
-						}
-					});
-			}
-
-			//if we have any textareas then fetch snippets
-			if ($("textarea").length > 0) {
-				var bug_id = 0;
-
-				$("form[name='bugnoteadd'] input[name='bug_id']").each(function() {
-						bug_id = $(this).val();
-					});
-				$("form[action='bug_update.php'] input[name='bug_id']").each(function() {
-						bug_id = $(this).val();
-					});
-
-				xhrurl = "xmlhttprequest.php?entrypoint=plugin_snippets"
-				if (bug_id > 0) {
-					xhrurl += "&bug_id=" + bug_id;
+						$(this).parent("td").removeClass("center");
 				}
 
-				xhr = $.getJSON(xhrurl, SnippetsUI);
+				} catch(e) {
+					alert(e);
+				}
+			});
+		}
+
+		//if we have any textareas then fetch snippets
+		if ($("textarea").length > 0) {
+			var bug_id = 0;
+
+			$("form[name='bugnoteadd'] input[name='bug_id']").each(function() {
+				bug_id = $(this).val();
+			});
+			$("form[action='bug_update.php'] input[name='bug_id']").each(function() {
+				bug_id = $(this).val();
+			});
+
+			var xhrurl = "xmlhttprequest.php?entrypoint=plugin_snippets";
+			if (bug_id > 0) {
+				xhrurl += "&bug_id=" + bug_id;
 			}
-		}
 
-		try {
-			SnippetsInit();
-		} catch(e) {
-			alert(e);
+			$.getJSON(xhrurl, SnippetsUI);
 		}
+	}
 
-	});
+	try {
+		SnippetsInit();
+	} catch(e) {
+		alert(e);
+	}
+
+});

commit 4036bb1280c7fe46a637d8c00a8ecf0f4e3ecb78

Only display selection list if there are snippets
Regression introduced by 0dfa8b19f9ffe356b99505575e723e6652cc1162.

Fixes #24

files/snippets.js
@@ -34,7 +34,8 @@ jQuery(document).ready(function($) {
 			$(data.selector).each(function() {
 				var textarea = $(this);
 
-				if (data.snippets !== null) {
+				// Only display snippets selector if there are any
+				if (data.snippets) {
 					try {
 						// Create Snippets select
 						var select = $("<select></select>");