8a50eb8e0130beaa3047cab4a576ff4a7497c40f
Victor Boctor <vboctor@gmail.com>
2023-08-26 20:15:53 -0700

    Implement SnippetGetCommand to get all private or all global snippets

--- Snippets.php
+++ Snippets.php
@@ -54,6 +54,7 @@ class SnippetsPlugin extends MantisPlugin
 
 	public function init() {
 		require_once( dirname( __FILE__ ) . '/core/Snippets.API.php' );
+		require_once( dirname( __FILE__ ) . '/core/commands/SnippetGetCommand.php' );
 		require_once( dirname( __FILE__ ) . '/core/commands/SnippetAddCommand.php' );
 		require_once( dirname( __FILE__ ) . '/core/commands/SnippetUpdateCommand.php' );
 		require_once( dirname( __FILE__ ) . '/core/commands/SnippetDeleteCommand.php' );
@@ -157,6 +158,7 @@ class SnippetsPlugin extends MantisPlugin
 				$t_app->get( '/data', [ $t_plugin, 'route_data' ] );
 				$t_app->get( '/data/{bug_id}', [ $t_plugin, 'route_data' ] );
 
+				$t_app->get( '/snippets', [ $t_plugin, 'snippet_get' ] );
 				$t_app->post( '/snippets', [ $t_plugin, 'snippet_add' ] );
 				$t_app->put( '/snippets/{snippet_id}', [ $t_plugin, 'snippet_update' ] );
 				$t_app->delete( '/snippets/{snippet_id}', [ $t_plugin, 'snippet_delete' ] );
@@ -266,6 +268,38 @@ class SnippetsPlugin extends MantisPlugin
 	}
 
 	/**
+	 * REST API for getting global or user specific snippets
+	 *
+	 * @param Slim\Http\Request  $p_request
+	 * @param Slim\Http\Response $p_response
+	 * @param array              $p_args
+	 * @return Slim\Http\Response
+	 */
+	public function snippet_get( $p_request, $p_response, $p_args ) {
+		plugin_push_current( $this->basename );
+
+		$t_query = array();
+
+		$t_global = $p_request->getParam( 'global' );
+		if( !is_null( $t_global ) ) {
+			$t_query['global'] = $t_global;
+		}
+
+		$t_data = array(
+			'query' => $t_query
+		);
+
+		$t_command = new SnippetGetCommand( $t_data );
+		$t_result = $t_command->execute();
+
+		plugin_pop_current();
+
+		return $p_response
+			->withStatus( HTTP_STATUS_SUCCESS )
+			->withJson( $t_result );
+	}
+
+	/**
 	 * REST API for searching accessible snippets.
 	 *
 	 * Caller can provide a search string `query` that will be matched for snippets
@@ -282,7 +316,7 @@ class SnippetsPlugin extends MantisPlugin
 		plugin_push_current( $this->basename );
 
 		$t_query_data = array(
-			'query' => $t_query = $p_request->getParam( 'query' )
+			'query' => $p_request->getParam( 'query' )
 		);
 
 		$t_limit = $p_request->getParam( 'limit' );
---
+++ core/commands/SnippetGetCommand.php
@@ -0,0 +1,76 @@
+<?php
+# Copyright (c) 2010 - 2012 John Reese
+# Copyright (c) MantisBT Team - mantisbt-dev@lists.sourceforge.net
+# Licensed under the MIT license
+
+require_once( dirname( __FILE__ ) . '/../Snippets.API.php' );
+
+use Mantis\Exceptions\ClientException;
+
+/**
+ * Command to get global snippets or snippets for a user.
+ */
+class SnippetGetCommand extends Command {
+	/**
+	 * The owner id
+	 *
+	 * @var int
+	 */
+	private $owner_id;
+
+	/**
+	 * Constructor
+	 *
+	 * @param array $p_data The command data.
+	 */
+	function __construct( array $p_data ) {
+		parent::__construct( $p_data );
+	}
+
+	/**
+	 * Validate the data.
+	 *
+	 * @return void
+	 * @throws ClientException
+	 */
+	protected function validate() {
+		$t_global = (int)$this->query( 'global', 0 );
+
+		if( $t_global ) {
+			$t_global_snippets_threshold = plugin_config_get( 'use_global_threshold', null, false, NO_USER );
+			if( !access_has_global_level( $t_global_snippets_threshold ) ) {
+				access_denied();
+			}
+
+			$this->owner_id = NO_USER;
+		} else {
+			$this->owner_id = auth_get_current_user_id();
+		}
+	}
+
+	/**
+	 * Execute the command.
+	 * @return array result
+	 */
+	protected function process() {
+		$t_snippets_result = array();
+
+		# global is always false, because we will expicitly include the global user id (NO_USER)
+		# or the current user id, as appropriate
+		$t_snippets = Snippet::load_by_type_user( 0, $this->owner_id, /* global */ false );
+
+		foreach( $t_snippets as $t_snippet ) {
+			$t_snippets_result[] = array(
+				'id'   => $t_snippet->id,
+				'name' => $t_snippet->name,
+				'text' => $t_snippet->value
+			);
+		}
+
+		$t_results = array(
+			'snippets' => $t_snippets_result,
+		);
+
+		return $t_results;
+	}
+}
