mantisbt-plugins/Snippets

commit d68818388258b3e2e69f24d2788fba85aeaf2863

Add types to Snippet class properties

core/Snippets.API.php
@@ -25,11 +25,30 @@ class Snippet
 	const PLACEHOLDER_HANDLER = '{handler}';
 	const PLACEHOLDER_PROJECT = '{project}';
 
-	public $id;
-	public $user_id;
-	public $type;
-	public $name;
-	public $value;
+	/**
+	 * @var int|null Snippet id
+	 */
+	public ?int $id;
+
+	/**
+	 * @var int User id
+	 */
+	public int $user_id;
+
+	/**
+	 * @var int Snippet type (We only use 0 for now)
+	 */
+	public int $type;
+
+	/**
+	 * @var string Snippet name
+	 */
+	public string $name;
+
+	/**
+	 * @var string Snippet value
+	 */
+	public string $value;
 
 	/**
 	 * Create a new Snippet object with the given details.

commit 5bd8b4e2d9229f593f2849f6abd39f4d72e6d210

Set default value for Snippet::$id
Fixes PHP warning when adding a new Snippet:
Typed property Snippet::$id must not be accessed before initialization.

Regression introduced by d68818388258b3e2e69f24d2788fba85aeaf2863.

core/Snippets.API.php
@@ -33,7 +33,7 @@ class Snippet
 	/**
 	 * @var int|null Snippet id
 	 */
-	public ?int $id;
+	public ?int $id = null;
 
 	/**
 	 * @var int User id